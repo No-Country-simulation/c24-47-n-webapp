8d823524f09a9139333f567ab477436addb0b599
Dieguidev <diegogaraycullas@gmail.com>
2025-02-16 14:36:13 -0500

    feat: Implementar controlador de autenticación con registro y inicio de sesión de usuarios

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+import { CustomError } from '../utils/custom.error';
+import { AuthService } from '../services';
+import { LoginUserDto, RegisterUserDto } from '../dto';
+
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+
+    console.log(`${error}`);
+
+    return res.status(500).json({ error: 'Internal Server Error' });
+  };
+
+  registerUser = (req: Request, res: Response) => {
+    const [error, registerUserDto] = RegisterUserDto.create(req.body);
+    if (error) {
+      res.status(400).json({ error });
+      return;
+    }
+
+    this.authService
+      .registerUser(registerUserDto!)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
+  };
+
+  loginUser = (req: Request, res: Response) => {
+    const [error, loginUserDto] = LoginUserDto.create(req.body);
+    if (error) {
+      res.status(400).json({ error });
+      return;
+    }
+
+    this.authService
+      .loginUser(loginUserDto!)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
+  };
+}
